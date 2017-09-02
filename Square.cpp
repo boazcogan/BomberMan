4586139e7a23c688b4731435fde2c75547fe0fac
boazcogan <boazcogan@yahoo.com>
2017-09-02 15:09:20 -0700

    Bomb Bug Fix

---
+++ Square.cpp
@@ -0,0 +1,10 @@
+#ifndef SQUARE_CPP
+#define SQUARE_CPP
+// This struct is designed to store information about a square on the gameBoard.
+struct Square {
+Square()	{val=5;Explodes = 0;}
+int Explodes;
+int val;
+};
+
+#endif
